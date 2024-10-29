34af93b10a391b33fae97a1be8d0f7790f2e3cf9
lukatinarelli <123157518+lukatinarelli@users.noreply.github.com>
2024-10-29 16:05:31 +0000

    Update

--- app.py
+++ app.py
@@ -17,36 +17,72 @@ def index():
 
 
 @app.route('/consultar', methods=['POST'])
-def consultar_tabla():
-    table_name = request.form['table_name']  # Obtener el nombre de la tabla del formulario
+def consultar_datos():
+    table_name = request.form['table_name_consultar']  # Obtener el nombre de la tabla del formulario
     conn = connect_db()
     cursor = conn.cursor()
     
+    # Obtener las columnas y su información
     columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
     
-    datos = cursor.execute(f"SELECT * FROM {table_name};").fetchall()  # Consulta todos los datos de la tabla seleccionada
+    # Obtener los datos de la tabla seleccionada
+    datos = cursor.execute(f"SELECT * FROM {table_name};").fetchall()
     conn.close()
 
-    # Construir la tabla HTML para devolverla
+    # Construir la tabla HTML
     tabla_html = '<h3>Datos de la Tabla: {}</h3>'.format(table_name)
     tabla_html += '<table><thead><tr>'
+
     if datos:
-        # Añadir encabezados de columna
+        # Añadir encabezados de columna y aplicar estilo si es Primary Key
         for col in columnas:
-            tabla_html += '<th>{}</th>'.format(col[1].capitalize())  # Cambia esto para mostrar nombres de columnas reales si es necesario   
+            col_name = col[1].capitalize()
+            is_primary_key = col[5] == 1  # Verificar si es Primary Key con el índice 5
+            if is_primary_key:
+                # Añadir clase "primary-key" para aplicar estilo especial
+                tabla_html += f'<th title="Clave primaria" class="primary-key">{col_name}</th>'
+            else:
+                tabla_html += f'<th>{col_name}</th>'
+        
         tabla_html += '</tr></thead><tbody>'
         
         # Añadir filas de datos
         for fila in datos:
             tabla_html += '<tr>'
             for valor in fila:
-                tabla_html += '<td>{}</td>'.format(valor)
+                tabla_html += f'<td>{valor}</td>'
             tabla_html += '</tr>'
         tabla_html += '</tbody></table>'
     else:
         tabla_html += '<p>No hay datos en esta tabla.</p>'
 
     return tabla_html  # Devolver el HTML generado
+
+
+@app.route('/insertar', methods=['POST'])
+def insertar_datos():
+    table_name = request.form['table_name_insertar']  # Obtener el nombre de la tabla del formulario
+    conn = connect_db()
+    cursor = conn.cursor()
+        
+    columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
+    conn.close()
+    
+    
+    insert_html = ''
+
+    for col in columnas:
+        col_name = col[1].capitalize()
+        is_primary_key = col[5] == 1  # Verificar si es Primary Key con el índice 5
+        
+        insert_html += f'<p>{col_name}: </p> <input type="text">'
+                
+    
+    insert_html += '</br> <input type="button" value="Insertar" />'
+  
+    return insert_html  # Devolver el HTML generado
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -9,25 +9,48 @@ function showSection(sectionId) {
 
     // Si la sección es "consultar-datos", restablece el select y limpia los resultados
     if (sectionId === 'consultar-datos') {
-        const tableSelect = document.getElementById('table_name');
+        const tableSelect = document.getElementById('table_name_consultar');
         tableSelect.selectedIndex = 0; // Restablece a la opción en blanco
         
         // Limpia los resultados
         const resultadosDiv = document.getElementById('resultados');
         resultadosDiv.innerHTML = ''; // Elimina el contenido de los resultados
+
+    } else if (sectionId === 'insertar-datos') {
+        const tableSelect = document.getElementById('table_name_insertar');
+        tableSelect.selectedIndex = 0; // Restablece a la opción en blanco
+        
+        // Limpia los resultados
+        const resultadosDiv = document.getElementById('insertar');
+        resultadosDiv.innerHTML = ''; // Elimina el contenido de los resultados
     }
 }
 
 $(document).ready(function(){
-    $('#table_name').change(function() {
+    $('#table_name_consultar').change(function() {
         var tableName = $(this).val(); // Obtener el valor seleccionado
 
         $.ajax({
             url: '/consultar',  // URL a la que se enviarán los datos
             type: 'POST',  // Método de la solicitud
-            data: { table_name: tableName },  // Enviar el nombre de la tabla
+            data: { table_name_consultar: tableName },  // Enviar el nombre de la tabla
             success: function(response) {
                 $('#resultados').html(response);  // Insertar la respuesta en el div "resultados"
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $('#table_name_insertar').change(function() {
+        var tableName = $(this).val(); // Obtener el valor seleccionado
+
+        $.ajax({
+            url: '/insertar',  // URL a la que se enviarán los datos
+            type: 'POST',  // Método de la solicitud
+            data: { table_name_insertar: tableName },  // Enviar el nombre de la tabla
+            success: function(response) {
+                $('#insertar').html(response);  // Insertar la respuesta en el div "resultados"
             }
         });
     });
