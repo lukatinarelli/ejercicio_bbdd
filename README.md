99577ab9fc745a077902136cd51beba5fa1c685c
Luka Ramon Tinarelli <123157518+lukatinarelli@users.noreply.github.com>
2024-10-31 14:34:39 +0000

    Update

---
+++ README.md
@@ -0,0 +1,50 @@
+# Mi Proyecto de Gestor de Bases de Datos
+
+## Descripción
+Este es mi primer proyecto grande, un gestor de bases de datos que permite a los usuarios realizar operaciones CRUD (Crear, Leer, Actualizar y Eliminar) sobre sus datos. La aplicación incluye:
+
+ -Interfaz gráfica: construida con HTML, CSS y JavaScript para una experiencia de usuario interactiva.
+ -Interfaz de texto: implementada en Python para acceder y gestionar los datos mediante comandos directos.
+
+Además, permite crear y eliminar tablas en la base de datos de forma sencilla.
+
+
+## Tabla de Contenidos
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Tecnologías Utilizadas](#tecnologías-utilizadas)
+- [Contribución](#contribución)
+- [Licencia](#licencia)
+- [Contacto](#contacto)
+
+
+## Instalación
+1. Clona el repositorio:
+   ```bash
+   git clone https://github.com/lukatinarelli/gestor_bbdd.git
+   cd gestor_bbdd
+
+2. Instala las dependencias:
+
+    pip install -r requirements.txt
+
+
+## Uso
+1. Para iniciar el servidor, ejecuta:
+    python main.py
+
+2. Accede a la aplicación: Entra en tu navegador a http://localhost:5000 para acceder a la interfaz gráfica.
+
+Si unicamente quiere usar la interfaz de texto:
+    python db_manager.py
+
+
+## Tecnologías Utilizadas
+ -Python: lenguaje base de la aplicación
+ -Flask: framework web para el backend
+ -SQLAlchemy: ORM para la gestión de la base de datos
+ -HTML/CSS/JavaScript: para la interfaz de usuario en el frontend
+
+
+Contribución
+¡Las contribuciones son bienvenidas! Como este es mi primer proyecto usando HTML/CSS/JavaScript junto con Python, pueden haber varios errores o mejoras posibles. Si encuentras algún error o tienes sugerencias, abre un issue o envía un pull request. Aprecio cualquier ayuda para mejorar el proyecto.
--- main.py
+++ main.py
@@ -1,264 +1,207 @@
-import os, sys
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import sqlite3
 
-conexion = sqlite3.connect("database.db")
-cursor = conexion.cursor()
+app = Flask(__name__)
 
+def connect_db():
+    return sqlite3.connect('database/database.db')  # Cambia el nombre de tu base de datos
 
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-
-while True:
-    tablas = [nombre[0] for nombre in cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';").fetchall()]
-    op = False
-
-    op = input("""¿Qué desea hacer?
-    1. Crear tabla
-    2. Eliminar tabla
-    3. Insertar datos
-    4. Consultar datos
-    5. Modificar datos
-    6. Eliminar datos
-    7. Salir
-Ingrese su opción: """)
-
-
-    if op == "1": # Crear tablas:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        nombre = input("¿Qué nombre quiere ponerle a la tabla? ")
-        
-        
-        lista_atributos = []
-        lista_atributos_tipos = [] 
-        
-        while True: # Guardar los atributos de la tabla nueva
-            os.system('cls' if os.name == 'nt' else 'clear')
-            
-            print(chr(27) + "[1;31m" + "Recuerda que el nombre de los atributos no puede tener espacios ni acentos." + chr(27) + "[;m")
-            atributo = input("Introduce los atributos que quieras de uno en uno (o escribe 'salir' para terminar): ")
-            
-            if atributo == "salir":
-                break 
-            
-            lista_atributos.append(atributo)
-        
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        
-        for i in lista_atributos: # Guardar el tipo de cada atributo
-            os.system('cls' if os.name == 'nt' else 'clear')
-            
-            print("¿Cuál es el tipo de dato de cada atributo? (texto o entero)")
-            tipo = input(f"{i} --> ")
-            
-            if tipo.lower() == "texto":
-                lista_atributos_tipos.append("TEXT")
-                
-            elif tipo.lower() == "entero":
-                lista_atributos_tipos.append("INTEGER")
-                
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        
-        # Hacer la sentencia para crear la tabla
-        sentencia = f"CREATE TABLE IF NOT EXISTS {nombre} ( id INTEGER PRIMARY KEY AUTOINCREMENT"
-        
-        for i in range(len(lista_atributos)):
-            sentencia += f", {lista_atributos[i]} {lista_atributos_tipos[i]}"
-        
-        sentencia += ");"
-        
-        # Ejecutar la sentencia
-        try:
-            cursor.execute(sentencia)
-            print(chr(27) + "[32m" + "La tabla se ha creado correctamente." + chr(27) + "[0m")  # Texto en verde
-
-        except sqlite3.Error as e:
-            print(chr(27) + "[31m" + f"Error al crear la tabla: {e}" + chr(27) + "[0m")  # Texto en rojo
-
-        
-        
-        if input("\nQuiéres hacer otra operación (S/N)? ").lower() == "s":
-            op = "0"
-        else:
-            op = "7"
-        
-        
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-    elif op == "2": # Eliminar tablas
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        print("¿Qué tabla desea eliminar?")
-        
-        x = 0
-
-        for i in tablas:
-            x += 1
-            
-            print(f"    {x}. {tablas[x - 1]}")
-            
-            
-        x = int(input("Ingrese su opción: "))
-        
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        
-        try:
-            # Ejecutar la consulta para eliminar la tabla
-            cursor.execute(f"DROP TABLE IF EXISTS {tablas[x - 1]}")
-            
-            # Confirmación de que la tabla fue eliminada
-            print(chr(27) + "[32m" + f"La tabla '{tablas[x - 1]}' ha sido eliminada correctamente." + chr(27) + "[0m")
-
-        except sqlite3.Error as e:
-            # Manejo de errores
-            print(chr(27) + "[31m" + f"Error al eliminar la tabla: {e}" + chr(27) + "[0m")
-        
-        
-        if input("\nQuiéres hacer otra operación (S/N)? ").lower() == "s":
-            op = "0"
-        else:
-            op = "7"
-        
-        
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-    elif op == "3": # Agregar información
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        print("¿En que tabla desea insertar información?")
-        
-        x = 0
-
-        for i in tablas:
-            x += 1
-            
-            print(f"    {x}. {tablas[x - 1]}")
-            
-            
-        x = int(input("Ingrese su opción: "))
-        
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-        
-        datos = []
-        columnas = cursor.execute(f"PRAGMA table_info({tablas[x - 1]})").fetchall()
-        comando = f"INSERT INTO {tablas[x - 1]} ("
-
-        # Recopilar los nombres de las columnas y los datos del usuario
-        for atributos in columnas:
-            datos.append(input(f"{atributos[1].capitalize()}: "))
-            comando += f'{atributos[1]}, '
-
-        # Eliminar la última coma y espacio
-        comando = comando[:-2]
-        comando += ') VALUES ('
-
-        # Añadir los placeholders para los valores
-        comando += ', '.join(['?'] * len(columnas))  # Crea una cadena con placeholders
-        comando += ')'
-        
-        
-        # Ejecutamos el comando para insertar usando placeholders
-        try:
-            cursor.execute(comando, datos)  # Pasa los datos como segundo argumento
-            print(chr(27) + "[32m" + "Registro insertado correctamente." + chr(27) + "[0m")
-        except sqlite3.Error as e:
-            print(chr(27) + "[31m" + f"Error al insertar el registro: {e}" + chr(27) + "[0m")
-        
-        
-        
-        if input("\nQuiéres hacer otra operación (S/N)? ").lower() == "s":
-            op = "0"
-        else:
-            op = "7"
-
-
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
-    elif op == "4":
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-
-        print("¿En que tabla desea consultar información?")
-        
-        x = 0
-
-        for i in tablas:
-            x += 1
-            
-            print(f"    {x}. {tablas[x - 1]}")
-
-        tbl = int(input("Ingrese su opción: "))
-
-
-        os.system('cls' if os.name == 'nt' else 'clear')
-
-
-        # Guardamos los atributos en una variable
-        columnas = cursor.execute(f"PRAGMA table_info({tablas[tbl - 1]})").fetchall()
-        
-        # Guardamos la información en una variable
-        resultados = cursor.execute(f"SELECT * FROM {tablas[tbl - 1]}").fetchall()
+@app.route('/')
+def index():
+    conn = connect_db()
+    cursor = conn.cursor()
+    tablas = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence';").fetchall() # Obtener las tablas
+    conn.close()
     
-        if resultados: # Comprobamos si hay información
-            print(chr(27) + "[32m" + f"Resultados encontrados:" + chr(27) + "[0m")
-                
-            z = "--------------------------------------------------------------------------\n"
-            
-            for i in range(len(resultados)):
-                z += f"{i + 1}. " 
-                
-                for j in range(len(columnas)):
-                    z += f"{columnas[j][1].capitalize()} = {resultados[i][j]}, "
-                
-                z = z[:-2]
-                z += "\n--------------------------------------------------------------------------\n"
-            
-            z = z[:-1]
-            print(z)
-        else:
-            print(chr(27) + "[33m" + f"No se encontraron registros." + chr(27) + "[0m")
-            
+    return render_template('index.html', tablas=tablas)
 
-        if input("\nQuiéres hacer otra operación (S/N)? ").lower() == "s":
-            op = "0"
-        else:
-            op = "7"
-            
-            
-        os.system('cls' if os.name == 'nt' else 'clear')
+
+@app.route('/consultar', methods=['POST'])
+def consultar_datos():
+    table_name = request.form['table_name_consultar']  # Obtener el nombre de la tabla del formulario
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    # Obtener las columnas y su información
+    columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
+    
+    # Obtener los datos de la tabla seleccionada
+    datos = cursor.execute(f"SELECT * FROM {table_name};").fetchall()
+    conn.close()
+
+    # Construir la tabla HTML
+    tabla_html = '<h3>Datos de la Tabla: {}</h3>'.format(table_name)
+    tabla_html += '<table><thead><tr>'
+
+    if datos:
+        # Añadir encabezados de columna y aplicar estilo si es Primary Key
+        for col in columnas:
+            col_name = col[1].capitalize()
+            is_primary_key = col[5] == 1  # Verificar si es Primary Key con el índice 5
+            if is_primary_key:
+                # Añadir clase "primary-key" para aplicar estilo especial
+                tabla_html += f'<th title="Clave primaria" class="primary-key">{col_name}</th>'
+            else:
+                tabla_html += f'<th>{col_name}</th>'
         
+        tabla_html += '</tr></thead><tbody>'
+        
+        # Añadir filas de datos
+        for fila in datos:
+            tabla_html += '<tr>'
+            for valor in fila:
+                tabla_html += f'<td>{valor}</td>'
+            tabla_html += '</tr>'
+        tabla_html += '</tbody></table>'
     else:
-        os.system('cls' if os.name == 'nt' else 'clear')
+        tabla_html += '<p>No hay datos en esta tabla.</p>'
 
-        print(chr(27) + "[1;31m" + "Opción inválida. Intente nuevamente\n" + chr(27) + "[;m")
+    return tabla_html  # Devolver el HTML generado
+
+
+@app.route('/insertar', methods=['POST'])
+def insertar_datos():
+    table_name = request.form['table_name_insertar']  # Obtener el nombre de la tabla del formulario
+    conn = connect_db()
+    cursor = conn.cursor()
         
+    columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
+    
+    insert_html = '<form id="inserta" action="/inserta" method="POST"> <h2>Insertar en la tabla: {}</h2>'.format(table_name)
 
-    if op == "7":
-        conexion.commit()
-        conexion.close()
+    insert_html += f'<input type="hidden" name="table_name_insertar" value="{table_name}">' # Nombre de la tabla
+
+    for col in columnas:
+        col_name = col[1].capitalize()
+        col_type = col[2].capitalize()
+        is_auto_increment = col[2].upper() == 'INTEGER' and col[5] == 1
         
-        sys.exit()
+        if is_auto_increment:
+            insert_html += f'<p>{col_name} (Auto Increment, no necesita valor)</p>'
+        else:
+            insert_html += f'<p>{col_name} ({col_type}):</p>'
+            insert_html += f'<input type="text" name="{col_name}" required><br>'
+                
+    insert_html += '</br> <input id="inserta_dato" type="submit" value="Insertar Datos" /> </form>'
+  
+    return insert_html  # Devolver el HTML generado
 
 
+@app.route('/inserta', methods=['POST'])
+def inserta_datos():
+    table_name = request.form['table_name_insertar']
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    # Obtener columnas de la tabla
+    columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
+    
+    # Preparar un diccionario para insertar los datos
+    data_to_insert = {}
+    
+    for col in columnas:
+        col_name = col[1].capitalize()
+        is_auto_increment = col[2].upper() == 'INTEGER' and col[5] == 1
+        
+        if not is_auto_increment:
+            data_to_insert[col_name] = request.form[col_name]
+    
+    columns = ', '.join(data_to_insert.keys())
+    placeholders = ', '.join(['?'] * len(data_to_insert))
+    sql = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+    
+    try:
+        cursor.execute(sql, tuple(data_to_insert.values()))
+        conn.commit()
+    except Exception as e:
+        return f"Error: {str(e)}", 400  # Manejo de errores
+    finally:
+        conn.close()  # Asegúrate de cerrar la conexión
+    
+    # Redirigir a la página principal después de la inserción
+    return redirect(url_for('index'))  # 'index' es el nombre de la función de tu página principal
 
 
-"""
-tablas
+@app.route('/eliminar', methods=['POST'])
+def eliminar_datos():
+    table_name = request.form['table_name_eliminar']  # Obtener el nombre de la tabla del formulario
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    # Obtener las columnas y su información
+    columnas = cursor.execute(f"PRAGMA table_info({table_name})").fetchall()
+    
+    # Obtener los datos de la tabla seleccionada
+    datos = cursor.execute(f"SELECT * FROM {table_name};").fetchall()
+    conn.close()
+
+    # Construir la tabla HTML
+    tabla_html = '<h3>Datos de la Tabla: {}</h3>'.format(table_name)
+    tabla_html += '<table><thead><tr>'
+
+    x = 0
+
+    if datos:
+        # Añadir encabezados de columna y aplicar estilo si es Primary Key
+        for col in columnas:
+            col_name = col[1].capitalize()
+            is_primary_key = col[5] == 1  # Verificar si es Primary Key con el índice 5
+            if is_primary_key:
+                # Añadir clase "primary-key" para aplicar estilo especial
+                tabla_html += f'<th title="Clave primaria" class="primary-key">{col_name}</th>'
+                x = columnas.index(col)
+            else:
+                tabla_html += f'<th>{col_name}</th>'
+        
+        
+        tabla_html += '<th>Eliminar registro</th></tr></thead><tbody>'
+        
+        # Añadir filas de datos
+        for fila in datos:
+            tabla_html += '<tr>'
+            for valor in fila:
+                tabla_html += f'<td>{valor}</td>'
+            tabla_html += f'<td><button title="Eliminar registro" onclick="elimina_datos({fila[x]}, \'{table_name}\', \'{x}\')" id="boton{fila[x]}">Eliminar</button></td></tr>'
+        tabla_html += '</tbody></table>'                          #
+    else:
+        tabla_html += '<p>No hay datos en esta tabla.</p>'
+
+    return tabla_html  # Devolver el HTML generado
 
 
-datos = [
-    {"Nombre": "Juan", "Edad": 25},
-    {"Nombre": "Luka", "Edad": 18}
-]
+@app.route('/elimina', methods=['POST'])
+def elimina_datos():
+    data = request.get_json()
+    id_registro = data.get('id')      # Obtiene el ID del registro
+    nombre_tabla = data.get('tabla')  # Obtiene el nombre de la tabla
+    columna = data.get('columna')  # Obtiene el nombre de la columna
+
+    if not id_registro or not nombre_tabla or not columna:
+        return jsonify({'status': 'error', 'message': 'ID, tabla o columna no proporcionados'}), 400
+
+    conn = connect_db()
+    cursor = conn.cursor()
+    
+    columnas = cursor.execute(f"PRAGMA table_info({nombre_tabla})").fetchall()
+    
+    try:
+        # Puedes usar la columna aquí si necesitas hacer algo específico con ella.
+        cursor.execute(f"DELETE FROM {nombre_tabla} WHERE {columnas[int(columna)][1]} = ?;", (id_registro,))
+        conn.commit()
+        
+        # Verificar si se eliminó algún registro
+        if cursor.rowcount == 0:
+            return jsonify({'status': 'error', 'message': 'Registro no encontrado'}), 404
+
+        return jsonify({'status': 'success', 'message': f'Registro {id_registro} eliminado de {nombre_tabla}'})
+    
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+    
+    finally:
+        conn.close()
 
 
-# Imprimir como una tabla
-print(f"{'No.':<5} {'Nombre':<10} {'Edad':<5}")
-for i, persona in enumerate(datos, start=1):
-    print(f"{i:<5} {persona['Nombre']:<10} {persona['Edad']:<5}")
-"""
+if __name__ == '__main__':
+    app.run(debug=True)
